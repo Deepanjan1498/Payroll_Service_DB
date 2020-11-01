bdfdbe6cbecd44ac3bdeb965d99963db9c604f49
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-11-01 23:02:02 +0530

    [Deepanjan]Add. To create a employee payroll table.

--- Payroll_Service_DB.sql
+++ Payroll_Service_DB.sql
@@ -2,3 +2,14 @@
 CREATE DATABASE payroll_service;
 USE  payroll_service;
 SELECT DATABASE();
+
+#UC2
+CREATE TABLE employee_payroll
+(	
+  id 			INT unsigned  NOT NULL AUTO_INCREMENT, 	
+  name 			VARCHAR(150)  NOT NULL,		 	
+  salary 		Double        NOT NULL,		 	
+  start 		DATE          NOT NULL,			 	
+  PRIMARY KEY 		(id)				 	
+);
+DESCRIBE employee_payroll; 
